b52c522e4ec20659ebc114ae9226921381f5d856
Lucyna Frank <lucyna@macbook-lucyna-frank.local>
2010-10-07 14:43:58 +0200

    pierwszy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stronka::Application.config.secret_token = '7f776cfd4575cea83293349c91aefd757fac9a28337fd1c2b71f014824fa2dafa0a20964f5710a786cb58ad19abab28a65edd47c137b9fb62ec5985fb3e20d7c'
